2a000bb0ebe990d6153dbe162bf30c2f5298a046
ashotjanibekyan <ashotjanibekyan@gmail.com>
2017-10-22 01:19:00 +0400

    չնչին

--- bottom.php
+++ bottom.php
@@ -1,2 +1,5 @@
+	<hr>
+	<footer id="footer">Թերությունների կամ առաջարկների դեպքում գրել <a href="https://hy.wikipedia.org/wiki/%D5%84%D5%A1%D5%BD%D5%B6%D5%A1%D5%AF%D6%81%D5%AB_%D6%84%D5%B6%D5%B6%D5%A1%D6%80%D5%AF%D5%B8%D6%82%D5%B4:%D4%B1%D5%B7%D5%B8%D5%BF%D5%8F%D5%86%D5%82">ԱշոտՏՆՂ</a>
+		 մասնակցին։<br><a href="https://github.com/ashotjanibekyan/wiki-elections">Կոդը ԳիթՀաբում</a></footer>
 	</body>
 </html>
